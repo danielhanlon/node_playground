dd5c6878822808d2461b457db9cff9d2249f17e9
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-06-26 17:20:31 +0100

    / ‘servefile.js’ - ‘#servefile.js#’ - ‘.#servefile.js’

--- servefile.js
+++ servefile.js
@@ -13,14 +13,17 @@ fs.stat(file_path, function(err,stat){
 	    'Content-Length': stat.size
 	});
 
+	//Pump pattern
 	var rs = fs.createReadStream(file_path);
 	rs.on('data', function(file_content){
-	    var flushed = response.write(file_content);	    
+	    var flushed = response.write(file_content);
+	    //If there's still data to be read then pause for now
 	    if (!flushed){
 		rs.pause();
 	    }
 	});
 	
+	//When the reponse stream has been emptied, resume
 	response.on('drain', function(){
 	    rs.resume();
 	});
