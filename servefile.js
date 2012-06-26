3c04e9722c6c0e90f446cd1b98c9ceafd3a195b3
Daniel James Hanlon <d.hanlon@ucl.ac.uk>
2012-06-26 17:03:26 +0100

    + ‘servefile.js’ - ‘.#servefile.js’ - ‘#servefile.js#’

--- servefile.js
+++ servefile.js
@@ -2,3 +2,20 @@ var http = require('http');
 var fs   = require('fs');
 
 var file_path = __dirname+'/Venice.jpg';
+fs.stat(file_path, function(err,stat){
+    if (err){
+	throw err;
+    }
+
+    http.createServer(function(request,response){
+	response.writeHead(200,{
+	    'Content-Type': 'image/jpeg',
+	    'Content-Length': stat.size
+	});
+
+	fs.readFile(file_path, funciton(err, file_content){
+	    response.write(file_content);
+	    response.end();
+	});
+    }).listen(4000);
+});
